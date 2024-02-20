4577354655354578df5aff0003f159b385701e7c
Laith Khireiwish <141301612+vincula1@users.noreply.github.com>
2024-02-20 01:57:21 +0000

    Homework 4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,35 @@
 //write include statements
+#include <iostream>
+#include "repetition.h"
+
+using namespace std;
 
 //write using statements
 
-int main() 
-{
+int main() {
+	int choice;
+	do {
+		cout << "1-Use Factorial Function\n2-Use GCD Function\n3-Exit\nEnter your choice here: ";
+		cin >> choice;
+		if (choice == 1) {
+			int num;
+			cout << "Enter a number ";
+			cin >> num;
+			cout << "Factorial: " << factorial(num) << endl;
+		} else if (choice == 2) {
+			int num1, num2;
+			cout << "Enter two numbers (Has to be formatted like- 15 5):" ;
+			cin >> num1 >> num2;
+			cout << "GCD: " << gcd(num1, num2) << endl;
+		} else if (choice == 3) {
+			char confirm;
+			cout << "Are you sure you want to exit? :3 (Reply with Y for yes)";
+			cin >> confirm;
+			if (confirm == 'Y') {
+				break;
+			}
+		}
+	} while (true);
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,27 @@
 //add include statements
+#include "repetition.h"
+
 
 //add function(s) code here
+
+int factorial(int num) {
+    int sum = 1;
+    while (num > 0) {
+        sum *= num;
+        num --;
+
+    }
+
+    return sum;
+}
+
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+
+    }
+    return a;
+}
